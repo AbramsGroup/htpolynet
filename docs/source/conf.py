a748cadacd90a891627b1505fac773083d2d7d2d
Cameron F. Abrams <cfa22@drexel.edu>
2026-03-26 14:36:14 -0400

    furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ language = 'en'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -67,12 +67,3 @@ html_static_path = ['_static']
 numfig = True
 autosummary_generate = True
 todo_include_todos = True
-
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
-    ]
-}
